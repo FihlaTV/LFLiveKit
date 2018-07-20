dc3b5d6b16f9a265f060b2c85a0c0443bdfc274e
Ogulcan <junkchorn@gmail.com>
2018-07-20 13:25:16 +0300

    Translated comments into English

--- LFLiveKit/configuration/LFLiveVideoConfiguration.h
+++ LFLiveKit/configuration/LFLiveVideoConfiguration.h
@@ -9,94 +9,96 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-/// 视频分辨率(都是16：9 当此设备不支持当前分辨率，自动降低一级)
+/// Video resolution(All 16:9. When this device does not support the current resolution, it automatically reduces one level.)
 typedef NS_ENUM (NSUInteger, LFLiveVideoSessionPreset){
-    /// 低分辨率
+    /// Low resolution
     LFCaptureSessionPreset360x640 = 0,
-    /// 中分辨率
+    /// Medium resolution
     LFCaptureSessionPreset540x960 = 1,
-    /// 高分辨率
+    /// High resolution
     LFCaptureSessionPreset720x1280 = 2
 };
 
-/// 视频质量
+/// Video quality
 typedef NS_ENUM (NSUInteger, LFLiveVideoQuality){
-    /// 分辨率： 360 *640 帧数：15 码率：500Kps
+    /// Resolution： 360 *640 number of frames：15 code rate：500Kps
     LFLiveVideoQuality_Low1 = 0,
-    /// 分辨率： 360 *640 帧数：24 码率：800Kps
+    /// Resolution： 360 *640 number of frames：24 code rate：800Kps
     LFLiveVideoQuality_Low2 = 1,
-    /// 分辨率： 360 *640 帧数：30 码率：800Kps
+    /// Resolution： 360 *640 number of frames：30 code rate：800Kps
     LFLiveVideoQuality_Low3 = 2,
-    /// 分辨率： 540 *960 帧数：15 码率：800Kps
+    /// Resolution： 540 *960 number of frames：15 code rate：800Kps
     LFLiveVideoQuality_Medium1 = 3,
-    /// 分辨率： 540 *960 帧数：24 码率：800Kps
+    /// Resolution： 540 *960 number of frames：24 code rate：800Kps
     LFLiveVideoQuality_Medium2 = 4,
-    /// 分辨率： 540 *960 帧数：30 码率：800Kps
+    /// Resolution： 540 *960 number of frames：30 code rate：800Kps
     LFLiveVideoQuality_Medium3 = 5,
-    /// 分辨率： 720 *1280 帧数：15 码率：1000Kps
+    /// Resolution： 720 *1280 number of frames：15 code rate：1000Kps
     LFLiveVideoQuality_High1 = 6,
-    /// 分辨率： 720 *1280 帧数：24 码率：1200Kps
+    /// Resolution： 720 *1280 number of frames：24 code rate：1200Kps
     LFLiveVideoQuality_High2 = 7,
-    /// 分辨率： 720 *1280 帧数：30 码率：1200Kps
+    /// Resolution： 720 *1280 number of frames：30 code rate：1200Kps
     LFLiveVideoQuality_High3 = 8,
-    /// 默认配置
+    /// Default allocation
     LFLiveVideoQuality_Default = LFLiveVideoQuality_Low2
 };
 
 @interface LFLiveVideoConfiguration : NSObject<NSCoding, NSCopying>
 
-/// 默认视频配置
+/// Default video configuration
 + (instancetype)defaultConfiguration;
-/// 视频配置(质量)
+/// Video configuration (quality)
 + (instancetype)defaultConfigurationForQuality:(LFLiveVideoQuality)videoQuality;
 
-/// 视频配置(质量 & 是否是横屏)
+/// Video configuration (quality & landscape is horizontal)
 + (instancetype)defaultConfigurationForQuality:(LFLiveVideoQuality)videoQuality outputImageOrientation:(UIInterfaceOrientation)outputImageOrientation;
 
 #pragma mark - Attribute
 ///=============================================================================
 /// @name Attribute
 ///=============================================================================
-/// 视频的分辨率，宽高务必设定为 2 的倍数，否则解码播放时可能出现绿边(这个videoSizeRespectingAspectRatio设置为YES则可能会改变)
+/// Video Resolution，
+// Be sure to set the width and height to a multiple of 2, otherwise green edges may appear during decoding playback.
+// (This one videoSizeRespectingAspectRatio Set to YES may change)
 @property (nonatomic, assign) CGSize videoSize;
 
-/// 输出图像是否等比例,默认为NO
+/// Whether the output image is proportional, the default is NO
 @property (nonatomic, assign) BOOL videoSizeRespectingAspectRatio;
 
-/// 视频输出方向
+/// Video output direction
 @property (nonatomic, assign) UIInterfaceOrientation outputImageOrientation;
 
-/// 自动旋转(这里只支持 left 变 right  portrait 变 portraitUpsideDown)
+/// Auto rotate (Only supported here left change right / portrait cahange portraitUpsideDown)
 @property (nonatomic, assign) BOOL autorotate;
 
-/// 视频的帧率，即 fps
+/// Video frame rate, ie fps
 @property (nonatomic, assign) NSUInteger videoFrameRate;
 
-/// 视频的最大帧率，即 fps
+/// Video maximum frame rate, ie fps
 @property (nonatomic, assign) NSUInteger videoMaxFrameRate;
 
-/// 视频的最小帧率，即 fps
+/// Video minimym frame rate, ie fps
 @property (nonatomic, assign) NSUInteger videoMinFrameRate;
 
-/// 最大关键帧间隔，可设定为 fps 的2倍，影响一个 gop 的大小
+/// Maximum keyframe interval, which can be set to 2 times fps, affecting the size of a gop
 @property (nonatomic, assign) NSUInteger videoMaxKeyframeInterval;
 
-/// 视频的码率，单位是 bps
+/// Videocode rate, the unit is bps
 @property (nonatomic, assign) NSUInteger videoBitRate;
 
-/// 视频的最大码率，单位是 bps
+/// Video maximum code rate，the unit is bps
 @property (nonatomic, assign) NSUInteger videoMaxBitRate;
 
-/// 视频的最小码率，单位是 bps
+/// Video最小code rate，the unit is bps
 @property (nonatomic, assign) NSUInteger videoMinBitRate;
 
-///< 分辨率
+///< Resolution
 @property (nonatomic, assign) LFLiveVideoSessionPreset sessionPreset;
 
-///< ≈sde3分辨率
+///< ≈sde3Resolution
 @property (nonatomic, assign, readonly) NSString *avSessionPreset;
 
-///< 是否是横屏
+///< Is it a landscape
 @property (nonatomic, assign, readonly) BOOL landscape;
 
 @end
