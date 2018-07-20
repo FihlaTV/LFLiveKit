59642b8d3f94450f23b41f30a2247f4858b8a50c
Ogulcan <junkchorn@gmail.com>
2018-07-20 13:18:20 +0300

    Translated comments into English

--- LFLiveKit/configuration/LFLiveAudioConfiguration.h
+++ LFLiveKit/configuration/LFLiveAudioConfiguration.h
@@ -8,66 +8,66 @@
 
 #import <Foundation/Foundation.h>
 
-/// 音频码率 (默认96Kbps)
+/// Audio bit rate (默认96Kbps)
 typedef NS_ENUM (NSUInteger, LFLiveAudioBitRate) {
-    /// 32Kbps 音频码率
+    /// 32Kbps Audio bit rate
     LFLiveAudioBitRate_32Kbps = 32000,
-    /// 64Kbps 音频码率
+    /// 64Kbps Audio bit rate
     LFLiveAudioBitRate_64Kbps = 64000,
-    /// 96Kbps 音频码率
+    /// 96Kbps Audio bit rate
     LFLiveAudioBitRate_96Kbps = 96000,
-    /// 128Kbps 音频码率
+    /// 128Kbps Audio bit rate
     LFLiveAudioBitRate_128Kbps = 128000,
-    /// 默认音频码率，默认为 96Kbps
+    /// Default Audio bit rate，The default is 96Kbps
     LFLiveAudioBitRate_Default = LFLiveAudioBitRate_96Kbps
 };
 
-/// 音频采样率 (默认44.1KHz)
+/// Audio sample rate (default 44.1KHz)
 typedef NS_ENUM (NSUInteger, LFLiveAudioSampleRate){
-    /// 16KHz 采样率
+    /// 16KHz Sampling rate
     LFLiveAudioSampleRate_16000Hz = 16000,
-    /// 44.1KHz 采样率
+    /// 44.1KHz Sampling rate
     LFLiveAudioSampleRate_44100Hz = 44100,
-    /// 48KHz 采样率
+    /// 48KHz Sampling rate
     LFLiveAudioSampleRate_48000Hz = 48000,
-    /// 默认音频采样率，默认为 44.1KHz
+    /// Default audio Sampling rate，The default is 44.1KHz
     LFLiveAudioSampleRate_Default = LFLiveAudioSampleRate_44100Hz
 };
 
-///  Audio Live quality（音频质量）
+///  Audio Live quality（Audio quality）
 typedef NS_ENUM (NSUInteger, LFLiveAudioQuality){
-    /// 低音频质量 audio sample rate: 16KHz audio bitrate: numberOfChannels 1 : 32Kbps  2 : 64Kbps
+    /// Low Audio quality audio sample rate: 16KHz audio bitrate: numberOfChannels 1 : 32Kbps  2 : 64Kbps
     LFLiveAudioQuality_Low = 0,
-    /// 中音频质量 audio sample rate: 44.1KHz audio bitrate: 96Kbps
+    /// Medium Audio quality audio sample rate: 44.1KHz audio bitrate: 96Kbps
     LFLiveAudioQuality_Medium = 1,
-    /// 高音频质量 audio sample rate: 44.1MHz audio bitrate: 128Kbps
+    /// High Audio quality audio sample rate: 44.1MHz audio bitrate: 128Kbps
     LFLiveAudioQuality_High = 2,
-    /// 超高音频质量 audio sample rate: 48KHz, audio bitrate: 128Kbps
+    /// Very High Audio quality audio sample rate: 48KHz, audio bitrate: 128Kbps
     LFLiveAudioQuality_VeryHigh = 3,
-    /// 默认音频质量 audio sample rate: 44.1KHz, audio bitrate: 96Kbps
+    /// Default Audio quality audio sample rate: 44.1KHz, audio bitrate: 96Kbps
     LFLiveAudioQuality_Default = LFLiveAudioQuality_High
 };
 
 @interface LFLiveAudioConfiguration : NSObject<NSCoding, NSCopying>
 
-/// 默认音频配置
+/// Default audio configuration
 + (instancetype)defaultConfiguration;
-/// 音频配置
+/// Audio configuration
 + (instancetype)defaultConfigurationForQuality:(LFLiveAudioQuality)audioQuality;
 
 #pragma mark - Attribute
 ///=============================================================================
 /// @name Attribute
 ///=============================================================================
-/// 声道数目(default 2)
+/// Number of channels(default 2)
 @property (nonatomic, assign) NSUInteger numberOfChannels;
-/// 采样率
+/// Sampling rate
 @property (nonatomic, assign) LFLiveAudioSampleRate audioSampleRate;
-/// 码率
+/// Code rate
 @property (nonatomic, assign) LFLiveAudioBitRate audioBitrate;
-/// flv编码音频头 44100 为0x12 0x10
+/// flv coded audio head 44100 为0x12 0x10
 @property (nonatomic, assign, readonly) char *asc;
-/// 缓存区长度
+/// Cache length
 @property (nonatomic, assign,readonly) NSUInteger bufferLength;
 
 @end
